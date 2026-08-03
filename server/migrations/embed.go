2a2c05128ca8abc6967dab57b7059dc3a459a666
zhiwilliam1-cell <zhiwilliam1@gmail.com>
2026-08-03 17:55:21 +0800

    fix(migrations): embed IELTS prompt upgrade

--- server/migrations/embed.go
+++ server/migrations/embed.go
@@ -49,4 +49,5 @@ import "embed"
 //go:embed 000061_agent_practice_handoffs.*.sql
 //go:embed 000062_agent_memory_extraction_context_barrier.*.sql
 //go:embed 000063_speech_feedback_acoustic_provider_boundary.*.sql
+//go:embed 000064_evaluation_ielts_speaking_prompt_v2.*.sql
 var Files embed.FS
